3daf27b9102f94ee49eb4a4b4ab562c59c810a84
mcerqueira1524 <116676066+mcerqueira1524@users.noreply.github.com>
2023-05-02 10:58:35 -0300

    Add files via upload

--- BD_Geral.py
+++ BD_Geral.py
@@ -7,8 +7,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.express as px
-import plotly.graph_objects as go
 import streamlit.components.v1 as components
 from PIL import Image
 from functools import reduce
@@ -16,6 +14,7 @@ import io
 from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
 from st_pages import Page, Section, add_page_title, show_pages
+
 # In[ ]:
 
 
