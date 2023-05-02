e582a439e28e57fde01928b151df2cef66fbddab
mcerqueira1524 <116676066+mcerqueira1524@users.noreply.github.com>
2023-05-02 10:54:48 -0300

    Add files via upload

--- BD_Geral.py
+++ BD_Geral.py
@@ -5,21 +5,25 @@
 
 
 import pandas as pd
-import streamlit.components.v1 as components
 import numpy as np
 import streamlit as st
-
-
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit.components.v1 as components
+from PIL import Image
+from functools import reduce
+import io
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
+from st_pages import Page, Section, add_page_title, show_pages
 # In[ ]:
 
 
 @st.experimental_memo
 
 def importar_base():
-    sts = pd.read_excel('I:/PROJETOS/ENSINO_SUPERIOR/3. Base de Dados/2022/Status/2022_Status_Universitarios.xlsm', sheet_name='BD_Geral',skiprows=6)
-    ra = pd.read_excel('I:/PROJETOS/ENSINO_SUPERIOR/3. Base de Dados/2022/RA/Acomp RA/RA_2022.1/Acomp_RA_2022_1.xlsm', sheet_name='Leitura_2022.1', skiprows=3)
-    empregabilidade =  pd.read_excel('I:/PROJETOS/ENSINO_SUPERIOR/9. Relações Institucionais/Indicadores/2022/2022_Painel_Empregabilidade_VP.xlsm', sheet_name='BD_Geral', skiprows=3)
-    return sts,ra,empregabilidade
+    bd = st.file_uploader("Upload do arquivo XLSX", type="xlsx")
+    return bd
 
 sts,ra,empregabilidade=importar_base()
 
@@ -27,50 +31,4 @@ sts,ra,empregabilidade=importar_base()
 # In[3]:
 
 
-juncao = pd.merge(sts,ra, on=['ID'], how="left")
-
-# In[5]:
-
-
-bd_completo = pd.merge(juncao, empregabilidade, left_on=['ID'], right_on=['RA'], how='left')
-
-
-# In[6]
-
-filtro = bd_completo.columns
-
-# In[7]
-
-st.sidebar.markdown('Indice:')
-st.sidebar.markdown('1 - Gênero')
-st.sidebar.markdown('2 - Raça')
-    
-# In[8]:
-
-genero = st.multiselect(
-          '1 - Gênero:',
-          options= bd_completo['Gênero'].dropna().unique(),
-          default= bd_completo['Gênero'].dropna().unique())
-    
-raca = st.multiselect(
-          ' 2 -Raça:',
-          options= bd_completo['Cor_Raça'].dropna().unique(),
-          default= bd_completo['Cor_Raça'].dropna().unique())
-
-colunas = st.multiselect(
-          'Colunas:',
-          options= bd_completo.columns.values.tolist())
-
-
-# In[10]:
-
-
-df_filtro = bd_completo.query('Gênero == @genero & Cor_Raça == @raca')
-
-# In[11]:
-
-st.dataframe(df_filtro[colunas])
-
-
-
-
+#juncao = pd.merge(sts,ra, on=['ID'], how="left")
